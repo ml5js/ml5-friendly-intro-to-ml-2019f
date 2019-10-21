691bbbdd3641a9ecb97917ba51fe8d9fa0300616
joeyklee <jsphknglee@gmail.com>
2019-10-21 07:39:45 -0400

    adds classification color example

--- examples/ex-5__nn-classification/sketch.js
+++ examples/ex-5__nn-classification/sketch.js
@@ -1,7 +1,56 @@
-function setup() {
-  createCanvas(400, 400);
+
+let neuralnet;
+let options = {
+  dataUrl:'data/colorData.json',
+  inputs: ['r', 'g', 'b'],
+  outputs: ['label'],
+  task:'classification',
+  debug: true
 }
 
-function draw() {
-  background(220);
+let resultP;
+
+let rSlider, gSlider, bSlider;
+
+function setup() {
+  createCanvas(400, 400);
+  resultP = createP('I predict...')
+
+  rSlider = createSlider(0, 255, 255)
+  gSlider = createSlider(0, 255, 0)
+  bSlider= createSlider(0, 255, 0)
+
+  neuralnet = ml5.neuralNetwork(options, dataLoaded)
+}
+
+function dataLoaded(){
+  neuralnet.normalizeData();
+
+  const trainingOptions = {
+    epochs: 24,
+    batchSize: 32
+  }
+  neuralnet.train(trainingOptions, doneTraining);
+
+}
+
+function doneTraining(){
+  predict()
+}
+
+function predict(){
+  const r = rSlider.value()
+  const g = gSlider.value()
+  const b = bSlider.value()
+
+  background(r, g, b);
+  neuralnet.predict({r, g, b}, gotResult)
+
+}
+
+function gotResult(err, result){
+  if(err) return;
+  
+  resultP.html(result[0].label);
+  predict();
 }
